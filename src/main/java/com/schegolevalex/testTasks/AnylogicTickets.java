6b4e28c8f8fedbbd8438faaf87ad7042481ccab0
Alexey Schegolev <schegolevalex@gmail.com>
2023-04-30 22:14:58 +0300

    add getFileFromResourceAsStream() and fix pom.xml

--- src/main/java/com/schegolevalex/testTasks/AnylogicTickets.java
+++ src/main/java/com/schegolevalex/testTasks/AnylogicTickets.java
@@ -8,9 +8,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -21,11 +19,14 @@ import java.util.List;
 public class AnylogicTickets {
 
     public static void main(String[] args) {
-        String filePath = "src/main/resources/tickets.json";
+        String resourceName = "tickets.json";
         String cityFrom = "Владивосток";
         String cityTo = "Тель-Авив";
 
-        StringBuilder json = getJsonStringFromFilePath(filePath);
+        AnylogicTickets app = new AnylogicTickets();
+
+        InputStream fileFromResourceAsStream = app.getFileFromResourceAsStream(resourceName);
+        StringBuilder json = getJsonStringFromInputStream(fileFromResourceAsStream);
         ObjectMapper mapper = getObjectMapper();
         JsonNode jsonNode;
         List<Ticket> tickets;
@@ -53,6 +54,17 @@ public class AnylogicTickets {
         return durationsList.stream().reduce(Duration.ZERO, Duration::plus).dividedBy(durationsList.size());
     }
 
+    private InputStream getFileFromResourceAsStream(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
+    }
+
     private static Duration get90Percentile(List<Ticket> tickets, String originName, String destinationName) {
         List<Ticket> filteredTicketList = getFilteredTicketList(tickets, originName, destinationName);
         List<Duration> durationsList = getDurationsList(filteredTicketList);
@@ -77,11 +89,11 @@ public class AnylogicTickets {
                 .toList();
     }
 
-    private static StringBuilder getJsonStringFromFilePath(String filePath) {
+    private static StringBuilder getJsonStringFromInputStream(InputStream inputStream) {
         StringBuilder json = new StringBuilder();
         String line;
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
             while ((line = bufferedReader.readLine()) != null) {
                 line = line.trim().replaceAll("\ufeff", "");
                 json.append(line);
